e8e76735a0e46e34a472f69b75bd0779a6a17756
Faiq Ahmed <43751075+FaiqAhmed73@users.noreply.github.com>
2020-12-30 12:43:17 +0500

    InfoPanel

--- my-react-project/src/Components/InfoPanel.js
+++ my-react-project/src/Components/InfoPanel.js
@@ -1,55 +1,70 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import { blue } from '@material-ui/core/colors';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-      maxWidth: 1000,
-      margin: '0 auto',
-      marginTop: 50,
+    maxWidth: 1000,
+    margin: '0 auto',
+    marginTop: 50,
   },
   paper: {
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
+  title: {
+    color: 'blue',
+  }
 }));
 
 export default function InfoPanel() {
   const classes = useStyles();
 
+  const [golobelData, SetGlobelData] = useState({});
+
   useEffect(() => {
-    async function getData(){
-        const response = await fetch
-        ("https://covid-19-data.p.rapidapi.com/report/totals?date=2020-07-21", {
+    async function getData() {
+      const response = await fetch
+        ("https://covid-19-data.p.rapidapi.com/totals", {
           "method": "GET",
           "headers": {
-              "x-rapidapi-key": "2f23d23171msh722c8c29ac571ecp117bcajsnd58cf9e07bc1",
-              "x-rapidapi-host": "covid-19-data.p.rapidapi.com"
+            "x-rapidapi-key": "2f23d23171msh722c8c29ac571ecp117bcajsnd58cf9e07bc1",
+            "x-rapidapi-host": "covid-19-data.p.rapidapi.com"
           }
-      })
-      
+        })
+
       let data = await response.json();
-          console.log(data);
+      // console.log(data[0]);
+      SetGlobelData(data[0]);
+      console.log(data[0]);
     }
     getData();
-})
+  }, [])
 
 
 
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper} elevation={3}>Grid 1</Paper>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper} elevation={3}>Grid 2</Paper>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Paper className={classes.paper} elevation={3}>Grid 3</Paper>
-        </Grid>
+        {Object.keys(golobelData).map(key => {
+          return (
+            <Grid item xs={12} sm={4}>
+              <Paper
+                className={classes.paper}
+                elevation={3}>
+                <h3 className= {classes.title}>
+                  {key.toUpperCase()}
+                  </h3>
+                <h3>{golobelData[key]}</h3>
+
+            </Paper>
+            </Grid>
+          )
+        })}
+
       </Grid>
     </div>
   );
